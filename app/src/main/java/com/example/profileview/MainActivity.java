b1799baee3e78b202b4184bdf844a10a10473cd9
Aghisna Riziq <aghisnariziq24@gmail.com>
2021-10-17 15:11:15 +0700

    1918037_profile

---
+++ app/src/main/java/com/example/profileview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.profileview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("wa.me/+6287794163130");
+    }
+}
